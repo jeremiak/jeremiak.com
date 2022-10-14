12ac16be19c6d0cbbccf1c0a283c04faa3f3b077
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2022-10-13 22:40:15 -0700

    Add an RSS feed (#24)
    * Add first attempt at RSS feed
    
    * Try changing RSS code
    
    * Change <?xml?>
    
    * Get RSS feed validation to pass

--- .eleventy.js
+++ .eleventy.js
@@ -1,44 +1,65 @@
+const cheerio = require('cheerio')
 const striptags = require('striptags')
 
-function extractExcerpt(page) {
-  // lifted from https://www.jonathanyeong.com/garden/excerpts-with-eleventy/
-  if (!page.hasOwnProperty("templateContent")) {
-    console.warn(
-      'Failed to extract excerpt: Document has no property "templateContent".'
-    );
-    return null;
-  }
+const host = 'https://www.jeremiak.com'
 
-  return striptags(page.templateContent)
-    .substring(0, 200) // Cap at 200 characters
-    .replace(/^\s+|\s+$|\s+(?=\s)/g, "")
-    .trim()
-    .concat("...");
+function encodeForHtml(s) {
+    const $ = cheerio.load(s)
+    $('embed').remove()
+    $('script').remove()
+    $('style').remove()
+    const characterEntities = $('body').html().replace(/</g, '&lt;').replace(/>/g, '&gt;')
+    const replacedUrls = characterEntities.replace(/\/img\//g, `${host}/img/`).replace(/\/blog\//g, `${host}/blog/`).replace(/\/data\//g, `${host}/data/`)
+
+    return replacedUrls
+}
+
+function dateToRfc822(d) {
+    const date = new Date(d)
+    return date.toUTCString()
+}
+
+function extractExcerpt(page) {
+    // lifted from https://www.jonathanyeong.com/garden/excerpts-with-eleventy/
+    if (!page.hasOwnProperty("templateContent")) {
+        console.warn(
+            'Failed to extract excerpt: Document has no property "templateContent".'
+        );
+        return null;
+    }
+
+    return striptags(page.templateContent)
+        .substring(0, 200) // Cap at 200 characters
+        .replace(/^\s+|\s+$|\s+(?=\s)/g, "")
+        .trim()
+        .concat("...");
 }
 
 function sortByOrder(values) {
-  let vals = [...values];     // this *seems* to prevent collection mutation...
-  return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
+    let vals = [...values]; // this *seems* to prevent collection mutation...
+    return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
 }
 
-module.exports = function (eleventyConfig) {
-  // Return your Object options:
+module.exports = function(eleventyConfig) {
+    // Return your Object options:
 
-  eleventyConfig.addFilter("byorder", sortByOrder);
+    eleventyConfig.addFilter("byorder", sortByOrder);
+    eleventyConfig.addFilter("dateToRfc822", dateToRfc822)
+    eleventyConfig.addFilter("safe", encodeForHtml);
 
-  eleventyConfig.addPassthroughCopy("data");
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("data");
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("js");
 
-  eleventyConfig.addShortcode("excerpt", extractExcerpt)
+    eleventyConfig.addShortcode("excerpt", extractExcerpt)
 
-  eleventyConfig.setBrowserSyncConfig({
-    files: './_site/css/*.css'
-  });
+    eleventyConfig.setBrowserSyncConfig({
+        files: './_site/css/*.css'
+    });
 
-  return {
-    dir: {
-      includes: '_includes',
+    return {
+        dir: {
+            includes: '_includes',
+        }
     }
-  }
 };
