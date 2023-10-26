c9215d77a404b3d7ccd85fd230127a6f556fc9f7
Jeremia Kimelman <jbkimelman@gmail.com>
2023-10-26 00:55:53 -0700

    try two layers

--- _posts/2023-10-26-percent-of-buildings.md
+++ _posts/2023-10-26-percent-of-buildings.md
@@ -4,6 +4,7 @@ title: What does it look like if 42% of the residential buildings are gone?
 
 Some text to make sure it's clear that it's markdown, does this push it off?
 
+
 <div id="map" style="height: 500px;"></div>
 
 <link rel="stylesheet" href="https://unpkg.com/maplibre-gl@3.3.1/dist/maplibre-gl.css">
--- js/percent-of-buildings.js
+++ js/percent-of-buildings.js
@@ -5,33 +5,48 @@ const PMTILES_URL = "https://deploy-preview-45--jeremiak-com.netlify.app/data/pe
 
 const p = new pmtiles.PMTiles(PMTILES_URL);
 protocol.add(p);
-p.getHeader().then((h) => {
-  const map = window.map = new maplibregl.Map({
-    container: "map",
-    zoom: 11.8,
-    center: [-121.45412, 38.57809],
-    style: {
-      version: 8,
-      sources: {
-        "buildings": {
-          type: "vector",
-          url: "pmtiles://" + PMTILES_URL,
-          attribution:
-            '© Microsoft',
-        },
+
+const map = window.map = new maplibregl.Map({
+  container: "map",
+  zoom: 11.8,
+  center: [-121.45412, 38.57809],
+  style: {
+    version: 8,
+    sources: {
+      "buildings": {
+        type: "vector",
+        url: "pmtiles://" + PMTILES_URL,
+        attribution:
+          '© Microsoft',
       },
-      layers: [
-        {
-          "id": "buildings",
-          "source": "buildings",
-          "source-layer": "sacramentopost",
-          "type": "fill",
-          "paint": {
-            "fill-color": "steelblue",
-          },
-        }
-      ],
     },
-  });
-  map.showTileBoundaries = true;
+    layers: [
+      {
+        "id": "buildings-sometimes",
+        "source": "buildings",
+        "source-layer": "sacramentopost",
+        "type": "fill",
+        "paint": {
+          "fill-color": "steelblue",
+          "fill-opacity": 1
+        },
+        "filter": ['==', ['get', 'removed'], true]
+      },
+      {
+        "id": "buildings-always",
+        "source": "buildings",
+        "source-layer": "sacramentopost",
+        "type": "fill",
+        "paint": {
+          "fill-color": "steelblue",
+        },
+        "filter": ['==', ['get', 'removed'], false]
+      }
+    ],
+    transition: {
+      duration: 300,
+      delay: 0
+    }    
+  },
 });
+map.showTileBoundaries = true;
